4b0363dab4a4a47a13955e58efa4afae567fe6c5
DangPham <94502923+DHPh@users.noreply.github.com>
2022-11-01 11:45:47 +0700

    Add body of CLOP

--- src/clop.cpp
+++ src/clop.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Clop init";
+    // Convert argv to string for query command line
+    // after convert you can check argv with key_args[int] - begin with 0
+    std::string current_exec_name = argv[0];
+    std::vector<std::string> key_args;
+    if (argc > 1)
+    {
+        key_args.assign(argv + 1, argv + argc);
+    }
+
+    /*
+        START BODY PROGRAM
+        where system switch to sub lib
+    */
+   
     return 0;
 }
